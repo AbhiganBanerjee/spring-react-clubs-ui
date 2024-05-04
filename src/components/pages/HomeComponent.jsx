8a116ee6ee86095c280948bc0a5ae049c427cf45
AbhiganBanerjee <abhigan.banerjee.20@aot.edu.in>
2024-05-05 01:48:39 +0530

    7th commit

--- src/components/pages/HomeComponent.jsx
+++ src/components/pages/HomeComponent.jsx
@@ -13,16 +13,12 @@ const HomeComponent = ()=>{
                 <h3 className="font-monospacem fw-bold"> <Home/> Welcome to Home Page</h3>
             </header>
             <section className="row">
-                <nav className='col-3 d-flex navbar navbar-expand-lg navbar-light' style={{backgroundColor:"inherit"}}>
-                    <div className="collapse navbar-collapse">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item active" style={{marginLeft:"20px"}}>
-                                <Link className='text-decoration-none text-white fw-bold fs-5' to="login" ><Button variant="contained" size="small" color="success" className="font-monospace fw-bold text-white">Login&nbsp;<Login/></Button></Link>
-                            </li>
-                            <li className="nav-item active" style={{marginLeft:"55px"}}>
-                                <Link className='text-decoration-none text-white fw-bold fs-5' to="register"><Button size="small" variant="contained" color="success" className="font-monospace fw-bold text-white">Register&nbsp;<HowToReg/></Button></Link>
-                            </li>
-                        </ul>
+                <nav className='col-3 d-flex' style={{backgroundColor:"inherit"}}>
+                    <div className="mt-2 d-flex" style={{marginLeft:"10px"}}>
+                        <Link className='text-decoration-none text-white fw-bold fs-5' to="login" ><Button variant="contained" size="small" color="success" className="font-monospace fw-bold text-white">Login&nbsp;<Login/></Button></Link>  
+                    </div>
+                    <div className="mt-2 d-flex" style={{marginLeft:"40px"}}>
+                    <Link className='text-decoration-none text-white fw-bold fs-5' to="register"><Button size="small" variant="contained" color="success" className="font-monospace fw-bold text-white">Register&nbsp;<HowToReg/></Button></Link>
                     </div>
                 </nav>
                 <main className="col-10 d-flex justify-content-center align-items-center" style={{height:"460px", marginLeft:"145px"}}>
